d17fbf9bdc50e40d90cca1cd9b0e50498c84a9da
Ishan <ishan101001@gmail.com>
2024-06-18 17:38:55 +0530

    Complete Project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# to-do-app
+This is a basic to-do application. In this application user can add its daily tasks. He/She can also add his tasks for future on a specific date.
+
+This application is built using HTML, CSS, Pyhton, and Jinja2 template.
+
+To run this application :: python -m flask --app app run
--- routes.py
+++ routes.py
@@ -4,29 +4,28 @@ import datetime
 
 pages = Blueprint("to-do", __name__, template_folder="templates", static_folder="static")
 
-
 @pages.context_processor
 def add_cal_date_range():
-    def date_range(start: datetime.datetime):
-        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+    def date_range(start):
+        if isinstance(start, str):
+            start = datetime.datetime.fromisoformat(start)
+        dates = [(start + datetime.timedelta(days=diff)).strftime('%Y-%m-%d') for diff in range(-3, 4)]
         return dates
     
-    return {"date_range": date_range}
+    return {"date_range": date_range, "datetime": datetime}
 
 def today_at_midnight():
     today = datetime.datetime.today()
-    return datetime.datetime(today.year, today.month, today.day)
+    return today.strftime('%Y-%m-%d')
 
 @pages.route("/")
 def index():
     date_str = request.args.get("date")
     if date_str:
-        selected_date = datetime.datetime.fromisoformat(date_str)
+        selected_date = datetime.datetime.fromisoformat(date_str).strftime('%Y-%m-%d')
     else:
         selected_date = today_at_midnight()
-
-    habit_on_date = current_app.db.todo.find({"added": {"$eq": selected_date}})
-    
+    habit_on_date = list(current_app.db.todo.find({"added": {"$eq": selected_date}}))
     completions = [
         habit['habit']
         for habit in current_app.db.completions.find({"date": selected_date})
@@ -46,14 +45,13 @@ def add():
                     "name": request.form.get("habit")
                 }
             )
-    return render_template("add.html", title="To-Do tracker -- Add Item", selected_date=datetime.datetime.today())
+        return redirect(url_for('to-do.index', date=selected_date))
+    return render_template("add.html", title="To-Do tracker -- Add Item", selected_date=today_at_midnight())
 
 @pages.post("/complete")
 def complete():
     date_string = request.form.get("date")
     habit = request.form.get("habitId")
-    date = datetime.datetime.fromisoformat(date_string)
-    current_app.db.completions.insert_one({"date": date, "habit": habit})
+    current_app.db.completions.insert_one({"date": date_string, "habit": habit})
 
     return redirect(url_for("to-do.index", date=date_string))
-
